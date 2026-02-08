8ae5e8f01559c830c1db62c50a69156267a638d0
Dana Halabi <d3hhalabi@gmail.com>
2026-02-08 09:49:00 +0300

    5 - Set up the chat route page

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the Next JS, from Chat id page!</h1>
+    </div>
+  );
+}
