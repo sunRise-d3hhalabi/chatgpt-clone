be2cb4472b80c17dcf051c8161affa957fdaa674
Dana Halabi <d3hhalabi@gmail.com>
2026-02-09 13:35:20 +0300

    30 - Create the getChats endpoint

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
+import { useEffect } from "react";
 
 export const ChatSidebar = () => {
+  useEffect(() => {
+    const loadChatList = async () => {
+      const response = await fetch(`/api/chat/getChatList`, {
+        method: "POST",
+      });
+      const json = await response.json();
+      console.log("CHAT LIST: ", json);
+    };
+    loadChatList();
+  }, []);
   return (
     <div className="bg-gray-900 text-white">
       <Link href="/api/auth/logout" className="btn">
