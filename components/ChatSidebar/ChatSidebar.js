93369c6f6dec60263ac4c57533255826ed8eccd9
Dana Halabi <d3hhalabi@gmail.com>
2026-02-09 13:49:59 +0300

    31 - Render the list of chats in the sidebar

--- components/ChatSidebar/ChatSidebar.js
+++ components/ChatSidebar/ChatSidebar.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
   useEffect(() => {
     const loadChatList = async () => {
       const response = await fetch(`/api/chat/getChatList`, {
@@ -9,14 +10,22 @@ export const ChatSidebar = () => {
       });
       const json = await response.json();
       console.log("CHAT LIST: ", json);
+      setChatList(json?.chats || []);
     };
     loadChatList();
   }, []);
   return (
-    <div className="bg-gray-900 text-white">
-      <Link href="/api/auth/logout" className="btn">
-        Logout
-      </Link>
+    <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
+      <Link href="/chat">New chat</Link>
+      <div className="flex-1 overflow-auto bg-gray-950">
+        {chatList.map((chat) => (
+          <Link key={chat._id} href={`/chat/${chat._id}`}>
+            {chat.title} <br/>
+          </Link>
+          
+        ))}
+      </div>
+      <Link href="/api/auth/logout">Logout</Link>
     </div>
   );
 };
